613c6b9d807ccb168aaf29e5be6e65d0ec34a148
Miguel Panuto <miguel.panuto@live.com>
2024-08-07 15:36:14 -0300

    feat: add makefile with python

--- Makefile
+++ Makefile
@@ -1,20 +1,20 @@
 nest_path = ./real-world/nestjs
 spring_path = ./real-world/spring
 
-k6_path = ./k6/real-world/
-
 nest-js:
 	echo "starting to test k6 with NestJS"
-	docker-compose -f  ${nest_path}/docker-compose.yml up -d --build
-	k6 run ${k6_path}/script-nestjs.js
+	docker-compose -f  ${nest_path}/docker-compose.yml up -d
 
-	pip3 install -r ./requirements.txt
-	python3 ./collect-data.py
+	python3 ./collect-data.py nest-container
 
 spring:
 	echo "starting to test k6 with Spring"
+	docker-compose -f  ${spring_path}/docker-compose.yml up -d
+
+	python3 ./collect-data.py spring-container
 
 all:
+	pip3 install -r ./requirements.txt
 	make nest-js
 	make spring
 
--- collect-data.py
+++ collect-data.py
@@ -1,15 +1,73 @@
 from sys import argv
-
+from os import path, remove
 import docker
+import asyncio
+import json
 
-def main():
-    container_name = argv[1]
+k6_path = path.join(path.dirname(__file__), "k6", "real-world")
 
+def get_k6_path(container_name: str):
+    if 'nest' in container_name:
+        return path.join(k6_path, "script-nestjs.js")
+    return path.join(k6_path, "script-spring.js")
+
+def parse_stats(stats):
+    return {
+        'cpu': stats['cpu_stats']['cpu_usage']['total_usage'],
+        'precpu': stats['precpu_stats']['cpu_usage']['total_usage'],
+        'memory': stats['memory_stats']['usage'],
+    }
+
+async def execute_k6(k6_script_path: str, event: asyncio.Event):
+    process = await asyncio.create_subprocess_exec(
+        "k6", "run", k6_script_path, "--quiet",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    if process.returncode == 0:
+        print(f"K6 execution completed: {stdout.decode()}")
+    else:
+        print(f"K6 execution failed: {stderr.decode()}")
+    
+    event.set()
+
+async def get_stats(container_name: str, event: asyncio.Event):
     client = docker.from_env()
     container = client.containers.get(container_name)
 
-    stats = container.stats(stream=False)
-    print(stats)
+    # delete file
+    if path.isfile("stats.json"):
+        remove("stats.json")
+
+    with open("stats.json", "w") as f:
+        f.write("[]")
+
+    while not event.is_set():
+        stats = container.stats(stream=False) # pyright: ignore
+        raw_file = ''
+        with open("stats.json", "r") as rf:
+            raw_file = rf.read()
+
+        with open("stats.json", "w") as f:
+            file = json.loads(raw_file)
+            file.append(parse_stats(stats))
+
+            f.write(json.dumps(file))
+
+        await asyncio.sleep(1)
+
+async def main():
+    container_name = argv[1]
+    k6_execution_path = get_k6_path(container_name)
+    
+    event = asyncio.Event()
+    
+    await asyncio.gather(
+        execute_k6(k6_execution_path, event),
+        get_stats(container_name, event)
+    )
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+
